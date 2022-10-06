725ba3a2093557f108a459c43212f2db20697657
Alberto Basalo <albertobasalo@hotmail.com>
2022-10-06 20:35:13 +0200

    feat: forkIn object

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,26 +1,45 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { Observable } from "rxjs";
+import { forkJoin, Observable } from "rxjs";
 import { data } from "../data.repository";
+import { Agency } from "../models/agency.interface";
 import { Trip } from "../models/trip.interface";
 import { ApiService } from "../services/api.service";
 
 @Component({
   selector: "app-home",
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.Default,
   template: `
     <app-agencies [agencies]="homeData.agencies"></app-agencies>
     <app-trips *ngIf="trips$ | async as trips" [trips]="trips"></app-trips>
     <app-reloading (reload)="onReload()"></app-reloading>
     <h1>{{ lang }}</h1>
+    <pre>{{ result | json }}</pre>
   `,
   styles: [],
 })
 export class HomePage {
   homeData = data;
+
   trips$!: Observable<Trip[]>;
+  agencies$!: Observable<Agency[]>;
+
   lang = "";
+  result: {
+    trips: any[];
+    agencies: any[];
+  } = {
+    trips: [],
+    agencies: [],
+  };
   constructor(private api: ApiService, route: ActivatedRoute) {
+    const requests$ = {
+      trips: this.api.getTrips$(),
+      agencies: this.api.getAgencies$(),
+    };
+
+    forkJoin(requests$).subscribe((response) => (this.result = response));
+
     this.lang = route.snapshot.data["lang"] || "";
     this.refreshWithOnPush();
   }
