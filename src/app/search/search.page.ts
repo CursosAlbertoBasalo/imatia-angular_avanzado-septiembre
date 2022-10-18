ae1f96516b0a418ba0d0de62d7a2326b2e3bb195
Alberto Basalo <albertobasalo@hotmail.com>
2022-10-18 18:33:11 +0200

    feat: rxjs

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { map, Observable, of, switchMap, tap } from "rxjs";
+import { Observable, of, Subject, switchMap } from "rxjs";
 import { Trip } from "../models/trip.interface";
 import { ApiService } from "../services/api.service";
 
@@ -15,23 +15,24 @@ import { ApiService } from "../services/api.service";
 export class SearchPage {
   searchTerm = "";
   trips$: Observable<Trip[]> = of([]);
-
+  searchTerm$ = new Subject<string>();
   constructor(route: ActivatedRoute, private api: ApiService) {
-    const searchTerm$ = route.queryParamMap.pipe(
-      map((params) => params.get("q") || ""),
-      tap((searchTerm) => (this.searchTerm = searchTerm)),
-      tap((searchTerm) => console.log("Searching for", this.searchTerm))
-    );
+    // this.searchTerm$ = route.queryParamMap.pipe(
+    //   map((params) => params.get("q") || ""),
+    //   tap((searchTerm) => (this.searchTerm = searchTerm)),
+    //   tap((searchTerm) => console.log("Searching for", this.searchTerm))
+    // );
     // ! 2
     // this.onSearchTermClassical(searchTerm$);
     // * 3
-    this.onSearchTermSwitched(searchTerm$);
+    this.onSearchTermSwitched();
   }
   public onSearchOutput(searchTerm: string) {
     // ! 1 every response is processed
     // this.searchTerm = searchTerm;
     // console.log("Searching for", this.searchTerm);
     // this.trips$ = this.api.getTripsByQuery$(this.searchTerm);
+    this.searchTerm$.next(searchTerm);
   }
 
   private onSearchTermClassical(searchTerm$: Observable<string>) {
@@ -41,9 +42,9 @@ export class SearchPage {
         (this.trips$ = this.api.getTripsByQuery$(searchTerm)),
     });
   }
-  private onSearchTermSwitched(searchTerm$: Observable<string>) {
+  private onSearchTermSwitched() {
     // * 3 switch map cancels pending requests and only repaints on latest results
-    this.trips$ = searchTerm$.pipe(
+    this.trips$ = this.searchTerm$.pipe(
       switchMap((searchTerm) => this.api.getTripsByQuery$(searchTerm))
     );
   }
