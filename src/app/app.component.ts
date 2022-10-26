c7cd730e47d80f3469ce0349547edb079fa8f149
Alberto Basalo <albertobasalo@hotmail.com>
2022-10-26 19:06:25 +0200

    feat: pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from "@angular/core";
 import { Title } from "@angular/platform-browser";
 import { NavigationEnd, Router } from "@angular/router";
-import { filter } from "rxjs";
+import { SwUpdate, VersionEvent } from "@angular/service-worker";
+import { filter, interval } from "rxjs";
 import { environment } from "src/environments/environment";
 
 @Component({
@@ -12,12 +13,35 @@ import { environment } from "src/environments/environment";
       <router-outlet></router-outlet>
     </main>
     <app-footer [title]="appTitle"></app-footer>
+    <pre>19:04</pre>
+    <section *ngIf="newVersion !== ''">
+      🔔 {{ newVersion }}
+      <button (click)="onClick()">♻️ Reload</button>
+    </section>
   `,
   styles: [],
 })
 export class AppComponent {
   appTitle = environment.title;
-  constructor(router: Router, titleService: Title) {
+  newVersion = "";
+
+  onClick() {
+    window.location.reload();
+  }
+
+  constructor(router: Router, titleService: Title, swUpdate: SwUpdate) {
+    swUpdate.versionUpdates.subscribe((event: VersionEvent) => {
+      if (event?.type === "VERSION_READY") {
+        const version = event.latestVersion;
+        this.newVersion = version.appData
+          ? JSON.stringify(version.appData)
+          : version.hash;
+      }
+    });
+
+    const oneMinute = 1000 * 60;
+    interval(oneMinute).subscribe(() => swUpdate.checkForUpdate());
+
     router.events
       .pipe(filter((event) => event instanceof NavigationEnd))
       .subscribe((event) => {
